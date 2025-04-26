0ebb734f88349ad387fd8e130f6724f742d12dd9
vaishnavi9542 <vaishuvadla1575@gmail.com>
2025-04-26 22:10:51 +0530

    Create reverse_pairs.py

---
+++ reverse_pairs.py
@@ -0,0 +1,32 @@
+def count_inversions(nums):
+     def merge_sort_and_count(nums, start, end):
+         if start >= end:
+             return 0
+ 
+         mid = (start + end) // 2
+         count = merge_sort_and_count(nums, start, mid) + merge_sort_and_count(nums, mid + 1, end)
+         j = mid + 1
+ 
+         for i in range(start, mid + 1):
+             while j <= end and nums[i] >2* nums[j]:
+                 j += 1
+                 count += (j - (mid + 1))
+ 
+         temp = []
+         left = start
+         right = mid + 1
+         while left <= mid and right <= end:
+             if nums[left] <= nums[right]:
+                 temp.append(nums[left])
+                 left += 1
+             else:
+                 temp.append(nums[right])
+                 right += 1
+ 
+         temp.extend(nums[left:mid + 1])
+         temp.extend(nums[right:end + 1])
+         nums[start:end + 1] = temp
+         return count
+     return merge_sort_and_count(nums,0,len(nums)-1)
+ nums=list(map(int,input().split()))
+ print(count_inversions(nums))

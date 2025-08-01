fc45a6666823494e4d53a415078c855ad11d1df0
vaishnavi9542 <vaishuvadla1575@gmail.com>
2025-08-01 15:16:46 +0530

    Create isAnagram.py

---
+++ isAnagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #return sorted(s)==sorted(t)
+        if len(s)!=len(t):
+            return False
+        count=[0]*26
+        for char in s:
+            count[ord(char)-ord('a')]+=1
+        for char in t:
+            if count[ord(char)-ord('a')]==0:
+                return False
+            count[ord(char)-ord('a')]-=1
+        return True
+
+    

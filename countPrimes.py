f4070a12f5ded2c2fd191ba5cc234a01af493e11
vaishnavi9542 <vaishuvadla1575@gmail.com>
2025-08-04 22:12:40 +0530

    Create countPrimes.py

---
+++ countPrimes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n<=2:
+            return 0
+        count=0
+        prime=[True]*n
+        prime[0]=prime[1]=False
+        for i in range(2,n):
+            if prime[i]==True:
+                count+=1
+                for j in range(i*i,n,i):
+                    prime[j]=False
+        return count
+        

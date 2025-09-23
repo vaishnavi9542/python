4799cc88d09998190e9a48299d041862788792a4
vaishnavi9542 <vaishuvadla1575@gmail.com>
2025-09-23 10:43:04 +0530

    Create lowerBound.py
    smallest element in an array which is greater than or equal to target

---
+++ lowerBound.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lowerBound(self, arr, target):
+        ans=len(arr)
+        low=0
+        high=len(arr)-1
+        while(low<=high):
+            mid=(low+high)//2
+            if (arr[mid]>=target):
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+        return ans
+        
